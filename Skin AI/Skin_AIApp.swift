69f084fdd273ce0a6478225410f65359db519b76
Shreyan Mitra <shreyan.mitra09@gmail.com>
2025-08-08 01:30:52 -0700

    Initial Commit

---
+++ Skin AI/Skin_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skin_AIApp.swift
+//  Skin AI
+//
+//  Created by Shreyan Mitra on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Skin_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
